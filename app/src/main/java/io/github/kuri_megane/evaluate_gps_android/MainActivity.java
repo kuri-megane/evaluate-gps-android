f35df8243b5b4c7bfe129e3e2104696930af1524
kuri.megane my_ubuntu <kuri.megane3060.4@gmail.com>
2019-08-18 00:14:34 +0900

    [FIX] リファクタリング
    パーミッションの権限要求処理を変更

--- app/src/main/java/io/github/kuri_megane/evaluate_gps_android/MainActivity.java
+++ app/src/main/java/io/github/kuri_megane/evaluate_gps_android/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
@@ -44,55 +45,53 @@ public class MainActivity extends AppCompatActivity {
         // Android 6, API 23以上でパーミッシンの確認
         if (Build.VERSION.SDK_INT >= 23) {
             checkMultiPermissions();
-        } else {
-            startLocationService();
         }
+
+        startLocationService();
 
     }
 
     // 位置情報許可の確認、外部ストレージのPermissionにも対応できるようにしておく
     private void checkMultiPermissions() {
-        // 位置情報の Permission
-        int permissionLocation = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION);
-        // 外部ストレージ書き込みの Permission
-        int permissionExtStorage = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
-        ArrayList reqPermissions = new ArrayList<>();
+        // 位置情報の Permission
+        int permissionLocation = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        );
+        // 外部ストレージ書き込みの Permission
+        int permissionExtStorage = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        );
+
+        ArrayList<String> reqPermissions = new ArrayList<>();
 
         // 位置情報の Permission が許可されているか確認
-        if (permissionLocation == PackageManager.PERMISSION_GRANTED) {
-            // 許可済
-        } else {
-            // 未許可
+        if (permissionLocation != PackageManager.PERMISSION_GRANTED) {
             reqPermissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
         }
 
         // 外部ストレージ書き込みが許可されているか確認
-        if (permissionExtStorage == PackageManager.PERMISSION_GRANTED) {
-            // 許可済
-        } else {
-            // 許可をリクエスト
+        if (permissionExtStorage != PackageManager.PERMISSION_GRANTED) {
             reqPermissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
         }
 
         // 未許可
         if (!reqPermissions.isEmpty()) {
-//            ActivityCompat.requestPermissions(this,
-//                    reqPermissions.toArray(new String[0]),
-//                    REQUEST_MULTI_PERMISSIONS);
-            // 未許可あり
-        } else {
-            // 許可済
-            startLocationService();
+            ActivityCompat.requestPermissions(
+                    this,
+                    reqPermissions.toArray(new String[0]),
+                    REQUEST_MULTI_PERMISSIONS
+            );
         }
     }
 
     // 結果の受け取り
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults
+    ) {
 
         if (requestCode == REQUEST_MULTI_PERMISSIONS) {
             if (grantResults.length > 0) {
@@ -100,31 +99,19 @@ public class MainActivity extends AppCompatActivity {
                     // 位置情報
                     if (permissions[i].
                             equals(Manifest.permission.ACCESS_FINE_LOCATION)) {
-                        if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-                            // 許可された
-
-                        } else {
-                            // それでも拒否された時の対応
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
                             toastMake("位置情報の許可がないので計測できません");
                         }
                     }
                     // 外部ストレージ
                     else if (permissions[i].
                             equals(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                        if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-                            // 許可された
-                        } else {
-                            // それでも拒否された時の対応
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
                             toastMake("外部書込の許可がないので書き込みできません");
                         }
                     }
                 }
-
-                startLocationService();
-
             }
-        } else {
-            //
         }
     }
 
